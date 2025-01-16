1c9fd69a7c6cd9f5aee766fdc928085d8f7f05e8
jinHanyoon <miracle7532@naver.com>
2025-01-16 16:07:32 +0900

    3d fix

--- src/component/header/page.jsx
+++ src/component/header/page.jsx
@@ -32,10 +32,7 @@ export default function Header() {
   }
     </div>
     </div>
-    <Routes>
-     <Route path="/main" element={<Main/>} />
-      <Route path="/login" element={<Login/>} />
-   </Routes> 
+
    </div>
   )
 }
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Main from './pages/main/page'
 
 
 export default function Layout({children}) {
   return (
     <div className='w-screen h-screen relative overflow-hidden overflow-x-hidden'>
-    <main>{children}</main>
+    <Main>{children}</Main>
     </div>
   )
 }
